12c169f2c988d24baf70185ee7e2e3eb67dfa433
revan <revansiddh.patil@roxiler.com>
2017-11-25 11:55:41 +0530

    movie list and detail

--- src/modules/movie/detail/screen.js
+++ src/modules/movie/detail/screen.js
@@ -5,20 +5,23 @@ import IoniIcons from 'react-native-vector-icons/Ionicons';
 import {stringConstant} from '../../../common/constants';
 
 const DetailScreen = (props) => {
-       const url = stringConstant.IMAGE_BASE_URL_BANNER.concat(props.data.poster_path);
-       console.log("boomer",url);
+       const posterPathUrl = stringConstant.IMAGE_BASE_URL_BANNER.concat(props.data.poster_path);
+       const bannerPathUrl = stringConstant.IMAGE_BASE_URL_BANNER.concat(props.data.backdrop_path)
+       console.log("boomer",posterPathUrl,bannerPathUrl);
        return (                         
         <Content>
-                <Card style={{flex:1}}>
-                <View style={{flex:1,flexDirection:'row',justifyContent:'center'}}>
+            <Card style={{flex:1}}>
+               <View style={{flex:1}}>
+                   <View style={{flex:1}}>
+                        <Image style={{height:175,flex:1}} source={{uri:bannerPathUrl}}/> 
+                   </View>
+                   <View style={{position:'relative',marginLeft:10,marginTop:-80,flex:1}}>                     
                         <Image             
-                                style={{height:stringConstant.SCREEN_HEIGHT/2,flex:1,alignSelf:'center'}}                   
-                                source={{uri:url}}/>  
-                        <View style={{flex:1}}>
-                        </View>   
-                </View>
-
-                </Card>       
+                          style={{height:200,width:150}}                   
+                          source={{uri:posterPathUrl}}/>   
+                   </View>                    
+               </View>
+            </Card>       
         </Content>
                 
         // <Image             
