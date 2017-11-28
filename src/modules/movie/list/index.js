02d1e11c99e285c3413451bb900b04ddaaf38ef9
revan <revansiddh.patil@roxiler.com>
2017-11-28 19:34:37 +0530

    done

--- src/modules/movie/list/index.js
+++ src/modules/movie/list/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import {Container, Content, View, Input, Text} from 'native-base';
+import {View, Input, Text,Fab, Button, Footer, FooterTab} from 'native-base';
 import { connect } from "react-redux";
 import {Spinner, PrimaryText} from '../../../common/components';
 import MoviList from '../list/list';
 import {StyleSheet} from 'react-native';
 import {fetchMovieList,searchMovieList} from './action';
 import { stringConstant } from '../../../common/constants/index';
+import IoniIcons from 'react-native-vector-icons/Ionicons';
 
 const listStyle = StyleSheet.create({
     textStyle : {
@@ -23,7 +24,7 @@ const listStyle = StyleSheet.create({
 });
 
 class CardContainer extends Component {
-    state={searchMovie:''}
+    state={searchMovie:'',filters:''}
     componentDidMount() {
         this.fetchList()   
     }
@@ -32,12 +33,36 @@ class CardContainer extends Component {
         this.props.fetchMovieList();
     }
    
-    render() {        
-        const message = this.state.searchMovie.length > 2 ? 'Searching movie...' : 'Loading movie...'
+    render() {   
+        const message = this.state.searchMovie.length > 2 ? 'Searching movie...' : 'Loading movie...';
+        let data=[];
+        if(this.state.filters)    {
+            data = this.props.list.list.map((l)=>{
+                if(this.state.filters == stringConstant.FAVORITE){
+                    if(this.props.list.favorites.includes(l.id)) {
+                        return l
+                    }
+                }else {
+                    if(this.props.list.bookmarks.includes(l.id)) {
+                        return l
+                    }
+                }  
+            }).filter(d=>d);
+            // return <MoviList 
+            //             catergories={this.props.list.catergories}
+            //             data={data}
+            //             onDetailPress={this.handleDetailPress.bind(this)}/>
+        }else {
+            if(this.state.searchMovie.length > 2 && this.props.list.searchList) {
+                data=this.props.list.searchList
+            }else {
+                data=this.props.list.list   
+            }
+        }
         let listData = !this.props.list.showLoader 
                         ?  <MoviList 
                                 catergories={this.props.list.catergories}
-                                data={(this.state.searchMovie.length > 2 && this.props.list.searchList) ? this.props.list.searchList : this.props.list.list}
+                                data={data}
                                 onDetailPress={this.handleDetailPress.bind(this)}/>
                         : <Spinner message={message}/>
         listData =  this.props.list.noData ? <Text style={listStyle.noDataFoundStyle}>No Movies Found</Text> :listData 
@@ -48,12 +73,20 @@ class CardContainer extends Component {
                     onChangeText={this.handleSearch.bind(this)} 
                     placeholder={stringConstant.SEARCH_PLACEHOLDER}
                     underlineColorAndroid="transparent"
-                    style={listStyle.textStyle}/>
+                    style={listStyle.textStyle}/>               
                <View style={{flex:8,justifyContent:'center'}}>
                    {listData}
-               </View>
+               </View>    
+               <FooterComponent filters={this.state.filters} onfilterPress={this.handleFilter.bind(this)}/>          
            </View>
         );
+    }
+    handleFilter(filters) {
+        if(this.state.filters == filters) {
+            this.setState({filters:''});
+        }else {
+            this.setState({filters:filters});
+        }
     }
 
     handleDetailPress(id,movieTitle) {
@@ -66,6 +99,27 @@ class CardContainer extends Component {
             this.props.searchMovieList(text);
         }
     }   
+}
+
+const FooterComponent = (props)=>{
+   return(
+            <Footer>
+                <FooterTab style={{backgroundColor:stringConstant.APP_BLACKGROUND_COLOR}}>
+                    <Button onPress={()=>props.onfilterPress(stringConstant.BOOKMARK)}>
+                        <IoniIcons 
+                            size={30} 
+                            color={props.filters == stringConstant.BOOKMARK ? stringConstant.APP_YELLOW_COLOR :stringConstant.APP_FONT_COLOR} 
+                            name="ios-bookmark"/>
+                    </Button>
+                    <Button  onPress={()=>props.onfilterPress(stringConstant.FAVORITE)}>
+                        <IoniIcons
+                             size={30} 
+                             color={props.filters == stringConstant.FAVORITE ? stringConstant.APP_YELLOW_COLOR :stringConstant.APP_FONT_COLOR} 
+                             name="md-heart"/>
+                    </Button>
+                </FooterTab>
+            </Footer>
+        )
 }
 const mapStateToProps = (state) =>({
     list : state.listReducer
