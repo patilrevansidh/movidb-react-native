f75dae11df8ab77575716564f7036ddff132d9eb
Patil Revansiddh <patilrevansiddh@gmail.com>
2018-03-19 13:22:31 +0530

    listing

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,8 @@ import List from './modules/movie/list';
 import MovieDetail from './modules/movie/detail';
 
 export const Root = StackNavigator({
-    Home : {screen:Home,navigationOptions:{header:null}},
-    MovieList :{screen:List,navigationOptions:{title:'Movies'}},
-    MovieDetail : {screen:MovieDetail}
+    Home : { screen: Home, navigationOptions: { header: null}},
+    MovieList: { screen: List, navigationOptions: { title: 'Movies'}},
+    MovieDetail: { screen: MovieDetail}
 
 });
